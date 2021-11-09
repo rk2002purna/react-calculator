6d51082477df74013b29641d9b3160d369c32080
rk2002purna <67547247+rk2002purna@users.noreply.github.com>
2021-11-09 23:31:53 +0530

    Add files via upload

---
+++ precache-manifest.63cf2c9f911d9b78e962c94039e37ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c5ab2cf8ae62b51bbef3de3f8b54f3",
+    "url": "/Calculator-using-ReactJS/index.html"
+  },
+  {
+    "revision": "2866f9d14bfcee897c36",
+    "url": "/Calculator-using-ReactJS/static/css/main.d9a02207.chunk.css"
+  },
+  {
+    "revision": "b273ef3d1d3e59b4d212",
+    "url": "/Calculator-using-ReactJS/static/js/2.dd77942f.chunk.js"
+  },
+  {
+    "revision": "2866f9d14bfcee897c36",
+    "url": "/Calculator-using-ReactJS/static/js/main.c88f9756.chunk.js"
+  },
+  {
+    "revision": "7ca014e22783c6e0d278",
+    "url": "/Calculator-using-ReactJS/static/js/runtime~main.5265f029.js"
+  }
+]);
